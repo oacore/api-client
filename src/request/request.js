fecfa981d873e5873bef401c2611bd405f135469
Jozef Harag <32jojo32@gmail.com>
2019-11-13 17:19:07 +0100

    Add support for /internal/articles/<id> endpoint

---
+++ src/request/request.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import { NetworkError } from './errors'
+
+const CORE_API = 'https://api.core.ac.uk/internal'
+// TODO: Add package version to UA
+const USER_AGENT = '@oacore/api'
+
+const apiRequest = async (url, method = 'GET', params = {}) => {
+  try {
+    const response = await axios({
+      url: `${CORE_API}${url}`,
+      method,
+      params,
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': USER_AGENT,
+      },
+    })
+
+    return { ...response.data, statusCode: response.status }
+  } catch (e) {
+    const { response, message } = e
+    let networkError
+    if (response) {
+      networkError = new NetworkError(
+        `Request for ${method} ${url} failed. Response: ${response.status}, ${response.data}`
+      )
+    } else if (message === 'Network Error') {
+      networkError = new NetworkError(
+        `Request ${method} ${url} failed. You are probably in offline mode.`
+      )
+    } else {
+      networkError = new NetworkError(
+        `Request ${method} ${url} failed. The original error was: ${message}`
+      )
+    }
+    networkError.statusCode = response.status || 500
+    throw networkError
+  }
+}
+
+export default apiRequest
