9161ed469ffab9aa1e743546f44ddc541a938d17
Jozef Harag <32jojo32@gmail.com>
2019-11-15 14:21:05 +0100

    Fix endpoint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import apiRequest from './request/request'
 
 class API {
   static getArticleMetadata(id) {
-    return apiRequest(`articles/${id}`)
+    return apiRequest(`/articles/${id}`)
   }
 }
 
