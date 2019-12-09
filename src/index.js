0ad623d191cedec6c0afdb6d0dc02bf18a6a3cd1
Joozty <32jojo32@gmail.com>
2019-12-09 17:30:46 +0100

    Add integrations endpoint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ class API {
   static getArticleMetadata(id) {
     return apiRequest(`/articles/${id}`)
   }
+
+  static getIntegrations(providerId) {
+    return apiRequest(`/data-providers/${providerId}/integrations`)
+  }
 }
 
 export default API
