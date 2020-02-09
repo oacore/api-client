0bd78c1109027170b7cf3eef22505e0eba09bad7
Viktor Yakubiv <viktor@yakubiv.com>
2020-02-09 13:03:38 +0200

    Enable most recent JavaScript features
    Adds support for:
    - async/await
    - class properties
    - nullish coalescing operator
    - optional chaining
    
    Installs:
    - @babel/runtime

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,12 @@ module.exports = api => {
   api.cache(true)
 
   const presets = [['@babel/preset-env']]
-  const plugins = []
+  const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime',
+  ]
 
   return {
     presets,
