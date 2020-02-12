ca333aff7baaa38abe0a1faed4805d069c1736ad
Viktor Yakubiv <viktor@yakubiv.com>
2020-02-12 17:10:29 +0200

    Improve entity tests

--- src/entity.test.js
+++ src/entity.test.js
@@ -1,46 +1,69 @@
 import Entity from './entity'
 
-const data = {
-  name: 'Entity 1',
-  updateDate: '2019-12-18T00:04:05Z',
-  downloadUrl: 'http://example.com',
-  url: 'http://example.com',
-}
+it('assigns properties itself', () => {
+  const initialState = {
+    id: 123,
+    url: 'https://example.com',
+  }
+  const entity = new Entity(initialState)
 
-const transforms = [
-  {
-    test: /Date$/,
-    transform: value => new Date(value),
-  },
-  {
-    test: /Url$/,
-    transform: value => new URL(value),
-  },
-]
-
-const options = { transforms }
-
-const entity = new Entity(data, options)
-
-it('has property from initial state', () => {
-  expect(entity.url).toBeDefined()
-  expect(entity.url).toBe(data.url)
+  expect(entity.id).toBe(initialState.id)
+  expect(entity.url).toBe(initialState.url)
 })
 
-it('transforms dates', () => {
-  expect(entity.updateDate).toBeInstanceOf(Date)
+describe('transforms', () => {
+  const initialState = {
+    name: 'Entity 1',
+    updateDate: '2019-12-18T00:04:05Z',
+    downloadUrl: 'http://example.com',
+    url: 'http://example.com',
+  }
+
+  const transforms = [
+    {
+      test: /Date$/,
+      transform: value => new Date(value),
+    },
+    {
+      test: /Url$/,
+      transform: value => new URL(value),
+    },
+  ]
+
+  const options = { transforms }
+
+  const entity = new Entity(initialState, options)
+
+  it('are applied if test matches', () => {
+    expect(entity.updateDate).toBeInstanceOf(Date)
+    expect(entity.downloadUrl).toBeInstanceOf(URL)
+  })
+
+  it('are not applied in test does not match', () => {
+    expect(entity.url).toBe(initialState.url)
+  })
 })
 
-it('transforms URLs', () => {
-  expect(entity.downloadUrl).toBeInstanceOf(URL)
-  expect(entity.url).not.toBeInstanceOf(URL)
-  expect(entity.url).toBe(data.url)
-})
-
-it('stores diffs', () => {
+describe('diff', () => {
+  const oldName = 'Entity 1'
   const newName = 'Entity 2'
 
-  expect(entity.diff).toEqual({})
-  entity.name = newName
-  expect(entity.diff).toEqual({ name: newName })
+  const initialState = {
+    name: oldName,
+  }
+  const entity = new Entity(initialState)
+
+  it('is empty after initialisation', () => {
+    expect(entity.diff).toEqual({})
+  })
+
+  it('is equal to changes happened', () => {
+    entity.name = newName
+    expect(entity.diff).toEqual({ name: newName })
+  })
+
+  it('is empty if changes undone', () => {
+    entity.name = oldName
+    expect(entity.diff).toEqual({})
+  })
 })
